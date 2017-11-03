627c77866b90ed65d2ecf359fcd8290b84f23a9f
mikrethor <mikrethor@gmail.com>
2017-11-02 22:49:55 -0400

    Adding entity and repository skeletons

---
+++ src/main/kotlin/com/ablx/daycare/backend/entity/Parent.kt
@@ -0,0 +1,11 @@
+package com.ablx.daycare.backend.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+internal data class Parent(
+        @field: Id @field: GeneratedValue var Id : Long = 0 //Primary Key
+
+)
